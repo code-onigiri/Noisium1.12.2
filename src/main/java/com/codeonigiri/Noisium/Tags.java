86bc79ba08fa563d1cb7f4bdaa3d8e8cc374f271
code-onigiri <shibamasa759@gmail.com>
2024-10-11 07:45:25 +0900

    init

---
+++ src/main/java/com/codeonigiri/Noisium/Tags.java
@@ -0,0 +1,12 @@
+package com.codeonigiri.Noisium;
+
+public class Tags {
+    private Tags() {}
+
+    /** Auto-generated tag from RetroFuturaGradle */
+    public static final String MOD_ID = "com.codeonigiri.noisium";
+    /** Auto-generated tag from RetroFuturaGradle */
+    public static final String MOD_NAME = "Noisium1.12.2";
+    /** Auto-generated tag from RetroFuturaGradle */
+    public static final String VERSION = "1.0.0";
+}
